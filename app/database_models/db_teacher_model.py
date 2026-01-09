afdfe9e43063cbd9fbfb7942b2f87f62f33e782a
B.G.ABILASH <abilashge15@gmail.com>
2026-01-09 19:25:09 +0530

    Update db_teacher_model.py

--- app/database_models/db_teacher_model.py
+++ app/database_models/db_teacher_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Enum, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
 class DBTeacherModel(Base):
