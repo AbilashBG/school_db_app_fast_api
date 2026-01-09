5e7601ee2548321893b8026bdaba63f04c489b60
B.G.ABILASH <abilashge15@gmail.com>
2026-01-09 23:54:06 +0530

    Gender enum updated

--- app/models/student_model.py
+++ app/models/student_model.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
 from datetime import date
+from enum import Enum
+
+class GenderEnum(str, Enum):
+    Male = "Male"
+    Female = "Female"
+    Other = "Other"
 
 class StudentModel(BaseModel):
     s_id: int
     name: str
     class_id: int
     dob: date  # Date in date format
-    gender: str  # 'Male', 'Female', 'Other'
+    gender: GenderEnum  # 'Male', 'Female', 'Other'
     fees_has_paid: bool
     
