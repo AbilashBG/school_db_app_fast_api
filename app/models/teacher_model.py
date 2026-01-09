b70cb33788904b825ad58fff95d028280d2cf51c
B.G.ABILASH <abilashge15@gmail.com>
2026-01-09 18:55:43 +0530

    Remove teacher models from student branch

--- app/models/teacher_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class TeacherModel(BaseModel):
-    t_id: int
-    name: str
-    subject: str
-    class_id: int
-    salary: float
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from database import Base, engine
 # Import all database models BEFORE creating tables
 from app.database_models.db_class_model import DBClassModel
 from app.database_models.db_student_model import DBStudentModel
-from app.database_models.db_teacher_model import DBTeacherModel
 
 app = FastAPI()
 
@@ -14,7 +13,6 @@ Base.metadata.create_all(bind=engine)
 # Import routes after tables are created
 from app.routes.class_routes import router as class_router
 from app.routes.student_routes import router as student_router
-from app.routes.teacher_rotes import router as teacher_router
 
 @app.get("/")
 def greet():
@@ -22,5 +20,4 @@ def greet():
 
 app.include_router(class_router)
 app.include_router(student_router)
-app.include_router(teacher_router)
 
